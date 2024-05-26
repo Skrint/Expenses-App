1bfc8a0cf491fd19834dc9ad98e06e947ac02af9
skrint <karelkin2004@gmail.com>
2024-05-26 20:34:31 +0300

    added: folder images, modal window, button reset, button edit, html element select with options, total price

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const LIMIT = 5000;
+let LIMIT = 5000;
 const CURRENCY = 'руб.';
 const STATUS_IN_LIMIT = 'Всё хорошо';
 const STATUS_OUT_OF_LIMIT = 'Всё плохо';
@@ -10,6 +10,31 @@ const $history = document.querySelector('.js-history');
 const $total = document.querySelector('.js-total');
 const $limit = document.querySelector('.js-limit');
 const $status = document.querySelector('.js-status');
+const $select = document.querySelector('.js-select');
+const $buttonReset = document.querySelector('.js-button-reset');
+const $buttonModal = document.querySelector('.js-button-modal');
+const $modalWindow = document.querySelector('.modal');
+const $closeModalWindow = document.querySelector('.close');
+const $buttonLimit = document.querySelector('.js-button-limit');
+const $inputLimit = document.querySelector('.js-input-limit');
+
+$buttonModal.addEventListener('click', function () {
+  $modalWindow.style.display = 'block';
+  $modalWindow.classList.add('modal-animation');
+  document.body.classList.add('modalBackdrop');
+});
+
+$closeModalWindow.addEventListener('click', function () {
+  $modalWindow.style.display = 'none';
+  $modalWindow.classList.remove('modal-animation');
+  document.body.style.backgroundColor = '#fff';
+  document.body.classList.remove('modalBackdrop');
+});
+
+$buttonLimit.addEventListener('click', () => {
+  LIMIT = $inputLimit.value;
+  init(expenses);
+});
 
 const expenses = [];
 
@@ -22,20 +47,32 @@ $button.addEventListener('click', () => {
     return;
   }
 
-  trackExpense(expense);
+  trackExpense(expense, $select);
   render(expenses);
 });
 
+function resetList(expenses) {
+  $buttonReset.addEventListener('click', () => {
+    $history.innerHTML = '';
+    expenses.length = 0;
+    $limit.innerHTML = LIMIT;
+    $total.innerHTML = calculateExpanses(expenses);
+    $status.innerHTML = STATUS_IN_LIMIT;
+    $status.classList.remove(STATUS_OUT_OF_LIMIT_CLASSNAME);
+  });
+}
+
 function init(expenses) {
+  resetList(expenses);
   $limit.innerHTML = LIMIT;
-  $status.innerHTML = STATUS_IN_LIMIT;
   $total.innerHTML = calculateExpanses(expenses);
+  $status.innerHTML = STATUS_IN_LIMIT;
 }
 
 function calculateExpanses(expenses) {
   let sum = 0;
   expenses.forEach((expense) => {
-    sum += expense;
+    sum += expense.expense;
   });
   return sum;
 }
@@ -47,8 +84,8 @@ function render(expenses) {
   renderStatus(sum);
 }
 
-function trackExpense(expense) {
-  expenses.push(expense);
+function trackExpense(expense, $select) {
+  expenses.push({ expense: expense, option: $select.value });
 }
 
 function getExpenseFromUser() {
@@ -71,7 +108,7 @@ function renderHistory(expenses) {
   let expensesListHTML = '';
 
   expenses.forEach((expense) => {
-    expensesListHTML += `<li>${expense} ${CURRENCY}</li>`;
+    expensesListHTML += `<li>${expense.expense} ${CURRENCY} - ${expense.option}</li>`;
   });
   $history.innerHTML = `<ol>${expensesListHTML}</ol>`;
 }
@@ -84,7 +121,9 @@ function renderStatus(sum) {
   if (sum <= LIMIT) {
     $status.innerHTML = STATUS_IN_LIMIT;
   } else {
-    $status.innerHTML = STATUS_OUT_OF_LIMIT;
+    $status.innerHTML = `${STATUS_OUT_OF_LIMIT} (${
+      LIMIT - $total.innerHTML
+    } ${CURRENCY})`;
     $status.classList.add(STATUS_OUT_OF_LIMIT_CLASSNAME);
   }
 }
